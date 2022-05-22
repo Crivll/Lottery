c1523c4d3a282c3008e4e54af11980ed7f890226
ljh <1225441967@qq.com>
2022-05-22 21:42:30 +0800

    05节,抽奖策略领域模块: 单匹配抽奖编写

--- Lottery/lottery-domain/src/main/java/com/ljh/lottery/domain/strategy/service/algorithm/impl/SingleRateRandomDrawAlgorithm.java
+++ Lottery/lottery-domain/src/main/java/com/ljh/lottery/domain/strategy/service/algorithm/impl/SingleRateRandomDrawAlgorithm.java
@@ -2,6 +2,7 @@ package com.ljh.lottery.domain.strategy.service.algorithm.impl;
 
 import com.ljh.lottery.domain.strategy.service.algorithm.BaseAlgorithm;
 
+import java.security.SecureRandom;
 import java.util.List;
 
 /**
@@ -14,6 +15,21 @@ import java.util.List;
 public class SingleRateRandomDrawAlgorithm extends BaseAlgorithm {
     @Override
     public String randomDraw(Long strategyId, List<String> excludeAwardIds) {
-        return null;
+
+        // 获取策略元组
+        String[] rateTuple = super.rateTupleMap.get(strategyId);
+        assert rateTuple != null;
+
+        // 随机索引
+        int randomVal = new SecureRandom().nextInt(100) + 1;
+        int idx = super.hashIdx(randomVal);
+
+        // 返回结果
+        String awardId = rateTuple[idx];
+        if (excludeAwardIds.contains(awardId)) {
+            return "未中奖";
+        }
+
+        return awardId;
     }
 }
